57d9a339aae8f9915ff752782e94981856b70930
kolawole olaniyi <kolaniyi3@gmail.com>
2023-03-03 18:38:22 +0100

    added encryption capability

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 const fs = require("fs");
+const crypto = require("crypto");
 
-const readStream = fs.createReadStream("./file.txt");
-const writeStream = fs.createWriteStream("./newfile.txt");
+const algorithm = "aes-256-cbc";
 
-readStream.pipe(writeStream);
+const initVector = crypto.randomBytes(16);
+const securityKey = crypto.randomBytes(32);
+
+const encryptData = () => {
+  const readStream = fs.readFileSync("./file.txt");
+
+  const message = readStream.toString("utf-8");
+
+  const cipher = crypto.createCipheriv(algorithm, securityKey, initVector);
+
+  let encryptedData = cipher.update(message, "utf-8", "hex");
+
+  encryptedData += cipher.final("hex");
+
+  fs.writeFileSync("./encryptedData.txt", encryptedData);
+  console.log("Encrypted message: " + encryptedData);
+};
+
+encryptData();
